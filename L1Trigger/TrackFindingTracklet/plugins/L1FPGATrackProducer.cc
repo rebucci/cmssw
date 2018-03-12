4066fb384f6358d3c709eaf28d647cd2a3bb888c
Louise Skinnari <louise.skinnari@cern.ch>
2018-03-12 13:53:43 +0100

    enable throwing out stubs failing FE inefficiencies

--- L1Trigger/TrackFindingTracklet/plugins/L1FPGATrackProducer.cc
+++ L1Trigger/TrackFindingTracklet/plugins/L1FPGATrackProducer.cc
@@ -613,15 +613,20 @@ void L1FPGATrackProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSe
       	strip=irphi[0];
       }
 
-      if (doMyDebug) std::cout << "... add this stub to the event!" << std::endl;
+      if (tempStubPtr->getTriggerDisplacement() > 100.) {
+	if (doMyDebug) std::cout << "... if FE inefficiencies calculated, this stub is thrown out! " << endl;
+      }
+      else {
+	if (doMyDebug) std::cout << "... add this stub to the event!" << std::endl;
+	if (ev.addStub(layer,ladder,module,strip,-1,tempStubPtr->getTriggerBend(),
+		       posStub.x(),posStub.y(),posStub.z(),
+		       innerStack,irphi,iz,iladder,imodule,isPSmodule,isFlipped)) {
+	  
+	  L1TStub lastStub=ev.lastStub();
+	  stubMap[lastStub]=tempStubPtr;
+	}
+      }
 
-      if (ev.addStub(layer,ladder,module,strip,-1,tempStubPtr->getTriggerBend(),
-		     posStub.x(),posStub.y(),posStub.z(),
-		     innerStack,irphi,iz,iladder,imodule,isPSmodule,isFlipped)) {
-
-	L1TStub lastStub=ev.lastStub();
-	stubMap[lastStub]=tempStubPtr;
-      }    
     }
   }
 
